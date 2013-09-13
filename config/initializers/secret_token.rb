74834b4334c9b3db8254c7ad2637043d4a7a214a
Dario Hunt <dariohunt@gmail.com>
2013-09-13 17:43:50 +0300

    Created a rail application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LLF::Application.config.secret_token = '97ff10d8fb121bad722ee7fb7c7c0a02a9bdc7ae1022ca4f03eaf282126a19b8d2e0bc8c5c8bacb1e0ea0bc7e3780b637e9937fb3d37576d797b94eb30bcc506'
