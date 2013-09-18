14c28c00f2375fce878a8da2c57d3126a5572468
Dario Hunt <dariohunt@gmail.com>
2013-09-19 00:53:48 +0300

    Scrapped previous implemented follow model and removed member_follows from db

--- app/models/member_follow.rb
+++
@@ -1,6 +0,0 @@
-class MemberFollow < ActiveRecord::Base
-  belongs_to :member
-  belongs_to :follow, class_name: 'Member', foreign_key: 'follow_id'
-
-  attr_accessible :member, :follow, :member_id, :follow_id
-end
