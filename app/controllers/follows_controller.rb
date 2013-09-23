08db69e9588e9fe89c054be0436e4ed8e0b7f538
Dario Hunt <dariohunt@gmail.com>
2013-09-24 02:03:35 +0300

    More changes/tweaks, added post method to follow/unfollow button

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,14 +1,14 @@
 class FollowsController < ApplicationController
 
-	before_filter :authenticate_member!, only: [:new, :create, :edit, :update]
+	before_filter :authenticate_member!
 
 	def create
-    	@member = Member.where(user_name: params[:id])
+    	@member = Member.find(params[:member_id])
     	current_member.follow(@member)
  	 end
 
   	def destroy
-    	@member = Member.where(user_name: params[:id])
+    	@member = Member.find(params[:member_id])
     	current_member.stop_following(@member)
 	end
 
