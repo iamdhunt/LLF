232df708e64966699372e2a100fc087d02fff3d5
Dario Hunt <dariohunt@gmail.com>
2013-09-24 16:33:21 +0300

    Folowing/Unfollowing now works for members, need to implement AJAX

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -5,11 +5,19 @@ class FollowsController < ApplicationController
 	def create
     	@member = Member.find(params[:member_id])
     	current_member.follow(@member)
- 	 end
+    	respond_to do |format|
+	      format.html { redirect_to @member }
+	      format.js { render :layout=>false, :content_type => 'application/javascript' }
+	    end
+ 	end
 
   	def destroy
     	@member = Member.find(params[:member_id])
     	current_member.stop_following(@member)
+    	respond_to do |format|
+	      format.html { redirect_to @member }
+	      format.js { render :layout=>false, :content_type => 'application/javascript' }
+	    end
 	end
 
 end
