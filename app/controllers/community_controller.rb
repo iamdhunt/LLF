4a1f0cc924541651b51758921122d263bac8b602
Dario Hunt <dariohunt@gmail.com>
2014-04-09 22:19:50 -0500

    Filtered community results

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,15 +1,15 @@
 class CommunityController < ApplicationController
 
   def community
-  	@activities = Activity.order("created_at desc").page(params[:page]).per_page(72)
+  	@activities = Activity.select("activities.*, COUNT(votes.id)").joins(:votes).group("activities.id").order("created_at desc").page(params[:page]).per_page(72)
   end
 
   def stream
-  	@activities = Activity.select("activities.*, COUNT(votes.id) vote_count").joins(:votes).order("created_at desc").page(params[:page]).per_page(72)
+  	@activities = Activity.select("activities.*, COUNT(votes.id)").joins(:votes).group("activities.id").order("created_at desc").page(params[:page]).per_page(72)
   end
 
   def media
-  	@media = Medium.select("media.*, COUNT(votes.id) vote_count").joins(:votes).order('created_at desc').page(params[:page]).per_page(60)
+  	@media = Medium.select("media.*, COUNT(votes.id) vote_count").joins(:votes).group("media.id").order('created_at desc').page(params[:page]).per_page(60)
   end
 
 end
