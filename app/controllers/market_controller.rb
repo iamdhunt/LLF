ffd79b36e3a627eb68060e0c681b149fafe7a687
Dario Hunt <dariohunt@gmail.com>
2014-05-31 01:39:46 -0500

    Styled market index

--- app/controllers/market_controller.rb
+++ app/controllers/market_controller.rb
@@ -1,6 +1,12 @@
 class MarketController < ApplicationController
 
 	def market
+		@brands = Brand.order("RANDOM()").limit(12)
+
+		respond_to do |format|
+	      format.html # index.html.erb
+	      format.json { render json: @brands }
+	    end
 	end 
 	
 end
