7fd7d19ac2f1cc3ad88efb6c76992ffdf90bd00a
Dario Hunt <dariohunt@gmail.com>
2014-06-04 16:40:14 -0500

    Small changes

--- app/controllers/market_controller.rb
+++ app/controllers/market_controller.rb
@@ -2,8 +2,8 @@ class MarketController < ApplicationController
 
 	def market
 		@member = Member.find(1)
-		@llf = @member.listings.order('created_at desc').limit(12)
-		@listings = Listing.order('created_at desc').limit(40)
+		@llf = @member.listings.order('created_at desc').limit(8)
+		@listings = Listing.order('created_at desc').limit(24)
 
 		respond_to do |format|
 	      format.html # index.html.erb
