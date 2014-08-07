aa978feb49497b88115572b793e11d3218f55310
Dario Hunt <dariohunt@gmail.com>
2014-08-07 00:59:26 -0500

    time zone fix

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
+config.time_zone = "Pacific Time (US & Canada)"
+
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 LLF::Application.initialize!
-
-config.time_zone = "Pacific Time (US & Canada)"
