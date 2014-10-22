f17658e3b431fcda31a431aaf9b65889b7b77dab
Dario Hunt <dariohunt@gmail.com>
2014-10-21 19:45:47 -0500

    and still working

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 LLF::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-	address: "mail.livinglifefearless.co",
-    port: 25,
-    domain: "l-l-f.co",
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["EMAIL_USERNAME"],
-    password: ENV["EMAIL_PASSWORD"],
-    openssl_verify_mode: 'none' 
-}
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.smtp_settings = {
+	address: "mail.livinglifefearless.co",
+    port: 25,
+    domain: "l-l-f.co",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"],
+    openssl_verify_mode: 'none' 
+}
