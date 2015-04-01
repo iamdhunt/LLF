c7c83350bb8a4c757bcba324effd83fa930c8f8c
Dario Hunt <dariohunt@gmail.com>
2015-04-01 11:40:30 -0700

    Removed duplicate migrations

--- db/migrate/20141110173943_create_mentions.rb
+++
@@ -1,9 +0,0 @@
-class CreateMentions < ActiveRecord::Migration
-  def change
-    create_table :mentions do |t|
-      t.belongs_to :mentionable, polymorphic: true
-      t.timestamps
-    end
-    add_index :mentions, [:mentionable_id, :mentionable_type]
-  end
-end
--- db/migrate/20150330222154_add_ids_to_mentions.rb
+++
@@ -1,7 +0,0 @@
-class AddIdsToMentions < ActiveRecord::Migration
-  def change
-    add_column :mentions, :status_id, :integer
-    add_column :mentions, :comment_id, :integer
-    add_column :mentions, :member_id, :integer
-  end
-end
