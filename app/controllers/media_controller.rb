6016076c6ca035c0a455cf2fec9ba6e1062c9489
Dario Hunt <dariohunt@gmail.com>
2016-02-20 18:37:18 +0100

    Adjusted status font sizing, lower cased all tags, added pagination to media index

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -19,7 +19,7 @@ class MediaController < ApplicationController
     @query = params[:media]
     @facet = params[:tag]
     @results = Medium.where(id: @search.results.map(&:id)).page(params[:page]).per_page(63)
-    @media = Medium.order_by_rand.limit(63).all
+    @media = Medium.order_by_rand.page(params[:page]).per_page(63)
 
     respond_to do |format|
       format.html # index.html.erb
