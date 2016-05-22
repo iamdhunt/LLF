8655d8f684cca132bf1d13a681e8bc1c883da126
Dario Hunt <dariohunt@gmail.com>
2016-05-21 23:46:29 -0700

    Mobile styling fixes

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -14,9 +14,16 @@ $(document).ready(function(){
        $( ".login_bg").css( "padding-top", "12%" );
        $( ".login_bg").css( "padding-bottom", "12%" );
        $( "#e_wrapper .nav-tabs li").css( "font-size", "1.3em" );
-       $( "#main_menu").css( "display", "none" );
-       $( "#main_menu2").css( "display", "inline" );
+       $( "#primary_nav").css( "display", "none" );
+       $( "#primary_nav2").css( "display", "inline" );
        $( ".fr-sticky-on").css( "top", "0px !important" );
+       $( "#custom-search-input input" ).css( "width", "85%" );
+       $( ".nav_search.l_o #custom-search-input" ).css( "margin-left", "190px" );
+       $( ".nav_search #custom-search-input" ).css( "margin-left", "190px" );
+       $( "#pb" ).css( "width", "98%" );
+       $( "#faqs_wrap" ).css( "padding-left", "1%" );
+       $( "#faqs_wrap" ).css( "padding-right", "1%" );
+       $( "#custom-search-input" ).css( "height", "36px" );
     }
 
 });
