037f44ba667944bb7f60c57ff786f305bf0d1aca
Dario Hunt <dariohunt@gmail.com>
2016-05-23 00:58:33 -0700

    Restyled activity stream items, fixed styling for small displays

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -7,23 +7,22 @@ $(document).ready(function(){
        $( "#e_wrapper" ).css( "width", "98%" );
        $( "#content_wrap" ).css( "width", "98%" );
        $( "#main_wrap" ).css( "width", "98%" );
-       $( "#pb" ).css( "padding-left", "15%" );
        $( "#home_wrapper" ).css( "background-image", "url('/assets/Registration 2.png')" );
-       $( "#faq_bg" ).css( "background-image", "url('/assets/FAQs Main 2.png')" );
+       $( "#e_404 #error_wrap" ).css( "background-image", "url('/assets/FAQs Main 2.png')" );
        $( ".login_bg" ).css( "background-image", "url('/assets/Login Banner 2.png')" );
        $( ".login_bg").css( "padding-top", "12%" );
        $( ".login_bg").css( "padding-bottom", "12%" );
        $( "#e_wrapper .nav-tabs li").css( "font-size", "1.3em" );
+       $( "#faqs_wrap .nav-tabs li").css( "font-size", "1.3em" );
        $( ".primary_nav").css( "display", "none" );
        $( ".primary_nav2").css( "display", "inline" );
        $( ".fr-sticky-on").css( "top", "0px !important" );
        $( "#custom-search-input input" ).css( "width", "85%" );
-       $( ".nav_search.l_o #custom-search-input" ).css( "margin-left", "190px" );
-       $( ".nav_search #custom-search-input" ).css( "margin-left", "190px" );
-       $( "#pb" ).css( "width", "98%" );
+       $( ".nav_search.l_o #custom-search-input" ).css( "margin-left", "205px" );
+       $( ".nav_search #custom-search-input" ).css( "margin-left", "205px" );
        $( "#faqs_wrap" ).css( "padding-left", "1%" );
        $( "#faqs_wrap" ).css( "padding-right", "1%" );
-       $( "#custom-search-input" ).css( "height", "36px" );
+       $( "#custom-search-input input" ).css( "font-size", "1em" );
     }
 
 });
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,8 @@
 module ActivitiesHelper
+
+	def act_profile_link(member, image_options={}, html_options={})
+	    avatar_url = member.avatar? ? member.avatar.url(:thumb) : "Default Av Thumb 4.png"
+	    link_to(image_tag(avatar_url, image_options), profile_path(member.user_name), html_options)
+	end
+
 end
