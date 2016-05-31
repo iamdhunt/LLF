5563fb4e1dc2793356682078711b76398e149101
Dario Hunt <dariohunt@gmail.com>
2016-05-31 02:06:22 -0700

    Trying to figure out the failed precompile

--- config/initializers/rails_client_tz_init.rb
+++
@@ -1,4 +0,0 @@
-# Say in config/initializers/rails_client_tz_init.rb
-RailsClientTimezone::Setting.mode = :smart #default value is :smart. Accepted values - :browser, :ip, :smart
-
-RailsClientTimezone::Setting.baseline_year = 2014 #default value is 2011. Accepted values - any valid year or string - "current"
