c04362084e6eae4dc829e61df13e914978fd9d0c
Dario Hunt <dariohunt@gmail.com>
2016-07-18 00:14:13 -0500

    Mobile menu

--- app/assets/javascripts/mobile.js
+++ app/assets/javascripts/mobile.js
@@ -14,8 +14,8 @@ $(document).ready(function(){
        $( "#faqs_wrap .nav-tabs li").css( "font-size", "1.3em" );
        $( ".fr-sticky-on").css( "top", "0px !important" );
        $( "#custom-search-input input" ).css( "width", "80%" );
-       $( ".nav_search.l_o #custom-search-input" ).css( "margin-left", "300px" );
-       $( ".nav_search #custom-search-input" ).css( "margin-left", "300px" );
+       $( ".nav_search.l_o #custom-search-input" ).css( "margin-left", "320px" );
+       $( ".nav_search #custom-search-input" ).css( "margin-left", "320px" );
        $( "#faqs_wrap" ).css( "padding-left", "1%" );
        $( "#faqs_wrap" ).css( "padding-right", "1%" );
        $( "#custom-search-input input" ).css( "font-size", "1em" );
